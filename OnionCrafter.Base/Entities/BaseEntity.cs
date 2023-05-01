b0fd604cb32b9a20c4b9f1f85eb71a12fbb77d89
dtopiast <dtopiast@protonmail.com>
2023-04-30 21:36:58 -0600

    Errors corrected in null assignments in constructors

--- OnionCrafter.Base/Entities/BaseEntity.cs
+++ OnionCrafter.Base/Entities/BaseEntity.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnionCrafter.Base.Entities
+﻿namespace OnionCrafter.Base.Entities
 {
     public abstract class BaseEntity<TKey> : IEntity<TKey>
     {
         public virtual TKey Id { get; set; }
 
-     
+        protected BaseEntity()
+        {
+            Id = Activator.CreateInstance<TKey>();
+        }
     }
+
     public abstract class BaseEntity : IEntity<string>
     {
         public virtual string Id { get; set; }
+
+        protected BaseEntity()
+        {
+            Id = string.Empty;
+        }
     }
 }
