1f962ce2627cb48f77deb2cf18bdc056b9f3ea22
dtopiast <dtopiast@protonmail.com>
2023-04-30 22:23:35 -0600

    Unused dependencies eliminated

--- OnionCrafter.Base/DTO/IAuditableDTO.cs
+++ OnionCrafter.Base/DTO/IAuditableDTO.cs
@@ -1,9 +1,4 @@
 ﻿using OnionCrafter.Base.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnionCrafter.Base.DTO
 {
