5446afaa82e172e9c335713c1743a5b698ffd174
dtopiast <dtopiast@protonmail.com>
2023-04-30 21:28:10 -0600

    Addition of BaseEntity and implementation of this to have a low-level non-generic inheritance for future implementations.

--- OnionCrafter.Base/Entities/IBaseEntity.cs
+++ OnionCrafter.Base/Entities/IBaseEntity.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Base.Entities
 {
-    public interface IEntity<TKey> : IBaseEntity
+    public class IBaseEntity
     {
-        public TKey Id { get; set; }
     }
 }
