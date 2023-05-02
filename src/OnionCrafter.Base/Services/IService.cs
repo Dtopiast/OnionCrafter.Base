ba47a50978ea58b41bb94b251cca1295dc606b81
dtopiast <dtopiast@protonmail.com>
2023-05-01 20:04:28 -0600

    Addition of _config property in IService<TServiceOptions>

--- src/OnionCrafter.Base/Services/IService.cs
+++ src/OnionCrafter.Base/Services/IService.cs
@@ -9,6 +9,7 @@ namespace OnionCrafter.Base.Services
     public interface IService<TServiceOptions> : IBaseService
         where TServiceOptions : IServiceOptions
     {
+        public TServiceOptions _config { get; }
     }
 
     public interface IService : IBaseService
