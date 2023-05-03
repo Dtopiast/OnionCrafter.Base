aad1429b789404d9ebd88103723e0397f21cb177
dtopiast <dtopiast@protonmail.com>
2023-05-03 17:21:05 -0600

    Correction of the implementation in the base inheritance of the DTO base class

--- src/OnionCrafter.Base/DTO/BaseRequestAuditableDTO.cs
+++ src/OnionCrafter.Base/DTO/BaseRequestAuditableDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace OnionCrafter.Base.DTO
 {
-    public abstract class BaseRequestAuditableDTO : IAuditableDTO
+    public abstract class BaseRequestAuditableDTO : BaseRequestDTO, IAuditableDTO
     {
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
--- src/OnionCrafter.Base/DTO/BaseResponseAuditableDTO.cs
+++ src/OnionCrafter.Base/DTO/BaseResponseAuditableDTO.cs
@@ -2,7 +2,7 @@
 
 namespace OnionCrafter.Base.DTO
 {
-    public abstract class BaseResponseAuditableDTO : IAuditableDTO, IResponseData
+    public abstract class BaseResponseAuditableDTO : BaseResponseDTO, IAuditableDTO
     {
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
