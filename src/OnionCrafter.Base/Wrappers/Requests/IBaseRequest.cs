87dc3449e5c19c3dade35b8155226ec2a6607687
dtopiast <dtopiast@protonmail.com>
2023-05-02 21:56:47 -0600

    Implementation of MediatR.IBaseRequest in IBaseRequest

--- src/OnionCrafter.Base/Wrappers/Requests/IBaseRequest.cs
+++ src/OnionCrafter.Base/Wrappers/Requests/IBaseRequest.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR.Wrappers;
 
 namespace OnionCrafter.Base.Wrappers.Requests
 {
-    public interface IBaseRequest
+    public interface IBaseRequest : MediatR.IBaseRequest
     {
     }
 }
