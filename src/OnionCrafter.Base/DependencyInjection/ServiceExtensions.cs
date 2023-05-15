01558ab81ba424e427612dcdf098a1170dfd90b6
dtopiast <dtopiast@protonmail.com>
2023-05-15 15:32:19 -0600

    Update and reimplementation of ServiceExtension

--- src/OnionCrafter.Base/DependencyInjection/ServiceExtensions.cs
+++ src/OnionCrafter.Base/DependencyInjection/ServiceExtensions.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using OnionCrafter.Base.Services;
 using OnionCrafter.Base.Services.Options;
 using System.Diagnostics.CodeAnalysis;
 
 namespace OnionCrafter.Base.DependencyInjection
 {
-    public static class ServiceExtension
+    public static class ServiceExtensions
     {
+        /// <summary>
+        /// Flag to indicate if the global service configuration has been setup.
+        /// </summary>
+        private static bool _isSetupGlobalServiceConfiguration;
+
         /// <summary> Adds a typed scoped service of type <typeparamref name="TService"/> with an
         /// implementation of type <typeparamref name="TImplementation"/> to the <see
         /// cref="IServiceCollection"/>. <typeparam name="TService">The type of the service to
@@ -34,8 +38,8 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection AddTypedScoped(this IServiceCollection services, Type TService, Type TImplementation)
         {
-            EnsureValidServiceTypes(TService, TImplementation);
-            services.AddScoped(TService, TImplementation);
+            services.AddTypedService(TService, TImplementation, ServiceLifetime.Scoped);
+
             return services;
         }
 
@@ -50,13 +54,13 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
         /// <param name="configure">A callback to configure the <typeparamref name="TOptions"/>.</param>
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-
         public static IServiceCollection AddTypedScoped<TService, TImplementation, TOptions>(this IServiceCollection services, Action<TOptions> configure)
-            where TService : IService<TOptions>
-            where TImplementation : class, TService
-            where TOptions : class, IServiceOptions
+                    where TService : IService<TOptions>
+                    where TImplementation : class, TService
+                    where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, typeof(TService), typeof(TImplementation), configure, ServiceLifetime.Scoped);
+            services.AddTypedScoped(typeof(TService), typeof(TImplementation), configure);
+            return services;
         }
 
         /// <summary>
@@ -73,7 +77,8 @@ namespace OnionCrafter.Base.DependencyInjection
         public static IServiceCollection AddTypedScoped<TOptions>(this IServiceCollection services, Type TService, Type TImplementation, Action<TOptions> configure)
             where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, TService, TImplementation, configure, ServiceLifetime.Scoped);
+            services.AddTypedService((TService), (TImplementation), ServiceLifetime.Scoped, configure);
+            return services;
         }
 
         /// <summary>
@@ -88,12 +93,35 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <param name="configure">A delegate to configure the <typeparamref name="TOptions"/>.</param>
         /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service to be added.</param>
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public static IServiceCollection AddTypedService<TOptions>(IServiceCollection services, Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implementationType, Action<TOptions> configure, ServiceLifetime lifetime)
+        public static IServiceCollection AddTypedService<TOptions>(this IServiceCollection services, Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implementationType, ServiceLifetime lifetime, Action<TOptions> configure)
             where TOptions : class, IServiceOptions
         {
+            services.AddOptions<TOptions>().Configure(configure);
+
+            services.AddTypedService(serviceType, implementationType, lifetime);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds a typed service of the type specified in <paramref name="serviceType"/> with a
+        /// factory specified in <paramref name="implementationType"/> to the <see
+        /// cref="IServiceCollection"/> with the given <paramref name="lifetime"/>.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of the options to be configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="serviceType">The type of the service to be added.</param>
+        /// <param name="implementationType">The type of the implementation to be used.</param>
+        /// <param name="configure">A delegate to configure the <typeparamref name="TOptions"/>.</param>
+        /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service to be added.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddTypedService(this IServiceCollection services, Type serviceType, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type implementationType, ServiceLifetime lifetime)
+
+        {
+            if (!_isSetupGlobalServiceConfiguration)
+                services.SetGlobalServiceConfiguration();
+
             EnsureValidServiceTypes(serviceType, implementationType);
 
-            services.AddOptions<TOptions>().Configure(configure);
             var descriptorService = new ServiceDescriptor(serviceType, implementationType, lifetime);
             services.Add(descriptorService);
             return services;
@@ -108,10 +136,10 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection AddTypedSingleton<TService, TImplementation>(this IServiceCollection services)
-                                                    where TService : IService
+            where TService : IService
             where TImplementation : class, TService
         {
-            services.AddSingleton(typeof(TService), typeof(TImplementation));
+            services.AddTypedSingleton(typeof(TService), typeof(TImplementation));
             return services;
         }
 
@@ -126,9 +154,7 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection AddTypedSingleton(this IServiceCollection services, Type TService, Type TImplementation)
         {
-            EnsureValidServiceTypes(TService, TImplementation);
-
-            services.AddTypedSingleton(TService, TImplementation);
+            services.AddTypedService(TService, TImplementation, ServiceLifetime.Singleton);
             return services;
         }
 
@@ -148,7 +174,8 @@ namespace OnionCrafter.Base.DependencyInjection
             where TImplementation : class, TService
             where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, typeof(TService), typeof(TImplementation), configure, ServiceLifetime.Singleton);
+            services.AddTypedSingleton(typeof(TService), typeof(TImplementation), configure);
+            return services;
         }
 
         /// <summary>
@@ -165,7 +192,8 @@ namespace OnionCrafter.Base.DependencyInjection
         public static IServiceCollection AddTypedSingleton<TOptions>(this IServiceCollection services, Type TService, Type TImplementation, Action<TOptions> configure)
             where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, TService, TImplementation, configure, ServiceLifetime.Singleton);
+            services.AddTypedService(TService, TImplementation, ServiceLifetime.Singleton, configure);
+            return services;
         }
 
         /// <summary>
@@ -192,8 +220,7 @@ namespace OnionCrafter.Base.DependencyInjection
         /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection AddTypedTransient(this IServiceCollection services, Type TService, Type TImplementation)
         {
-            EnsureValidServiceTypes(TService, TImplementation);
-            services.AddTransient(TService, TImplementation);
+            services.AddTypedService(TService, TImplementation, ServiceLifetime.Transient);
             return services;
         }
 
@@ -213,7 +240,8 @@ namespace OnionCrafter.Base.DependencyInjection
             where TImplementation : class, TService
             where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, typeof(TService), typeof(TImplementation), configure, ServiceLifetime.Transient);
+            services.AddTypedTransient(typeof(TService), typeof(TImplementation), configure);
+            return services;
         }
 
         /// <summary>
@@ -230,7 +258,58 @@ namespace OnionCrafter.Base.DependencyInjection
         public static IServiceCollection AddTypedTransient<TOptions>(this IServiceCollection services, Type TService, Type TImplementation, Action<TOptions> configure)
             where TOptions : class, IServiceOptions
         {
-            return AddTypedService(services, TService, TImplementation, configure, ServiceLifetime.Transient);
+            services.AddTypedService(TService, TImplementation, ServiceLifetime.Transient, configure);
+            return services;
+        }
+
+        /// <summary>
+        /// Sets the global service configuration for the specified type.
+        /// </summary>
+        /// <typeparam name="TGlobalServiceConfiguration">The type of the global service configuration.</typeparam>
+        /// <param name="services">The service collection.</param>
+        /// <param name="globalServiceConfiguration">The global service configuration.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection SetGlobalServiceConfiguration<TGlobalServiceConfiguration>(this IServiceCollection services, Action<TGlobalServiceConfiguration> globalServiceConfiguration, string configName)
+            where TGlobalServiceConfiguration : class, IGlobalServiceOptions
+        {
+            if (!_isSetupGlobalServiceConfiguration)
+            {
+                services.AddOptions<TGlobalServiceConfiguration>(configName).Configure(globalServiceConfiguration);
+                Environment.SetEnvironmentVariable("GlobalServiceConfiguration", configName);
+                _isSetupGlobalServiceConfiguration = true;
+            }
+            else
+                throw new InvalidOperationException("Global service configuration has already been set up.");
+            return services;
+        }
+
+        /// <summary>
+        /// Sets the global service configuration with the given options.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="globalServiceConfiguration">The global service configuration.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection SetGlobalServiceConfiguration(this IServiceCollection services)
+        {
+            var Options = new GlobalServiceOptions();
+            var globalServiceConfiguration = (GlobalServiceOptions options) =>
+            {
+                options.UseLogger = true;
+            };
+            services.SetGlobalServiceConfiguration(globalServiceConfiguration);
+            return services;
+        }
+
+        /// <summary>
+        /// Sets the global service configuration.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="globalServiceConfiguration">The global service configuration.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection SetGlobalServiceConfiguration(this IServiceCollection services, Action<GlobalServiceOptions> globalServiceConfiguration)
+        {
+            services.SetGlobalServiceConfiguration(globalServiceConfiguration);
+            return services;
         }
 
         /// <summary>
