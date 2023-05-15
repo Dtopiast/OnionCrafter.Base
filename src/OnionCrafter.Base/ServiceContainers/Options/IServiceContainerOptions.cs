a0881ffdd8d6e0f427bc26f15cd1652bb5d8c7dc
dtopiast <dtopiast@protonmail.com>
2023-05-14 21:35:10 -0600

    Created, implemented and documented the classes and interfaces of the ServiceContainers folder.

---
+++ src/OnionCrafter.Base/ServiceContainers/Options/IServiceContainerOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace OnionCrafter.Base.ServiceContainers.Options
+{
+    /// <summary>
+    /// Provides options for configuring a service container, including whether to use a logger and logger options.
+    /// </summary>
+    /// <typeparam name="TLoggerOptions">The type of logger options to use.</typeparam>
+    public interface IServiceContainerOptions<TLoggerOptions>
+    where TLoggerOptions : class, IServiceContainerLoggerOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to use a logger.
+        /// </summary>
+        bool UseLogger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logger options to use.
+        /// </summary>
+        public TLoggerOptions LoggerOptions { get; set; }
+    }
+}
