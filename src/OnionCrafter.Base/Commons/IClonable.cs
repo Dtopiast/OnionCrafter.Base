25432dcedd5428a99666b5c279ebb6a074c9de10
dtopiast <dtopiast@protonmail.com>
2023-05-14 21:27:53 -0600

    Creation and documentation of IClonable<T>

---
+++ src/OnionCrafter.Base/Commons/IClonable.cs
@@ -0,0 +1,17 @@
+﻿namespace OnionCrafter.Base.Commons
+{
+    /// <summary>
+    /// Represents an interface for cloning objects of a specific type.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to be cloned.</typeparam>
+    public interface IClonable<T>
+    {
+        /// <summary>
+        /// Creates a shallow copy of the object.
+        /// </summary>
+        /// <typeparam name="TReturn">The type of the cloned object.</typeparam>
+        /// <returns>A new instance of the cloned object.</returns>
+        TReturn Clone<TReturn>()
+            where TReturn : T;
+    }
+}
