461773b604f6264e90bc09944ad0216331e86232
dtopiast <dtopiast@protonmail.com>
2023-05-14 21:24:10 -0600

    Change of implementation and documentation of IActionTrace<TKey>.

---
+++ src/OnionCrafter.Base/Commons/IActionTrace.cs
@@ -0,0 +1,13 @@
+﻿namespace OnionCrafter.Base.Commons
+{
+    /// <summary>
+    /// Interface for tracing actions with a generic key.
+    /// </summary>
+    public interface IActionTrace<TKey>
+    {
+        /// <summary>
+        /// Set or get the tracing action Id
+        /// </summary>
+        public TKey ActionId { get; set; }
+    }
+}
