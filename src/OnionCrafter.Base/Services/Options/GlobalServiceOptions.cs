a3de9fb2e554a35738650089531a73ac1917524f
dtopiast <dtopiast@protonmail.com>
2023-05-15 11:21:28 -0600

    Creation and implementation of IGlobalServiceOptions

---
+++ src/OnionCrafter.Base/Services/Options/GlobalServiceOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Base.Services.Options
+{
+    /// <summary>
+    /// Represents the global service options.
+    /// </summary>
+    public class GlobalServiceOptions : IGlobalServiceOptions
+    {
+        /// <summary>
+        ///  /// Gets or sets a value indicating whether to use a logger.
+        /// </summary>
+        public bool UseLogger { get; set; }
+    }
+}
---
+++ src/OnionCrafter.Base/Services/Options/IGlobalServiceOptions.cs
@@ -0,0 +1,11 @@
+﻿using OnionCrafter.Base.Commons;
+
+namespace OnionCrafter.Base.Services.Options
+{
+    /// <summary>
+    /// Interface for providing global service options.
+    /// </summary>
+    public interface IGlobalServiceOptions : IUseLogger
+    {
+    }
+}
