7cf6d459fe4784d1c8d97ae21cefc146dd0a8e0d
dtopiast <dtopiast@protonmail.com>
2023-05-15 17:31:08 -0600

    Addition of exceptions

---
+++ src/OnionCrafter.Base/Exceptions/InvalidDTOCastException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Base.Exceptions
+{
+    /// <summary>
+    /// Represents an exception that is thrown when an invalid cast is attempted on a DTO object.
+    /// </summary>
+    [Serializable]
+    public class InvalidDTOCastException : Exception
+    {
+        /// <summary>
+        /// Exception thrown when an invalid DTO cast is attempted.
+        /// </summary>
+        /// <returns>
+        /// An InvalidDTOCastException object.
+        /// </returns>
+        public InvalidDTOCastException()
+        { }
+
+        /// <summary>
+        /// Constructor for InvalidDTOCastException class.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        /// <returns>
+        /// An instance of InvalidDTOCastException.
+        /// </returns>
+        public InvalidDTOCastException(string message) : base(message) { }
+
+        /// <summary>
+        /// Creates an instance of InvalidDTOCastException with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        /// <returns>An instance of InvalidDTOCastException.</returns>
+        public InvalidDTOCastException(string message, Exception inner) : base(message, inner) { }
+
+        /// <summary>
+        /// Represents an exception that is thrown when an invalid cast is attempted on a DTO object.
+        /// </summary>
+        protected InvalidDTOCastException(
+               System.Runtime.Serialization.SerializationInfo info,
+               System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ src/OnionCrafter.Base/Exceptions/InvalidSchemaException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Base.Exceptions
+{
+    /// <summary>
+    /// Represents an exception that is thrown when an invalid schema is encountered.
+    /// </summary>
+    [Serializable]
+    public class InvalidSchemaException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the InvalidSchemaException class.
+        /// </summary>
+        /// <returns>A new instance of the InvalidSchemaException class.</returns>
+        public InvalidSchemaException()
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the InvalidSchemaException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <returns>A new instance of the InvalidSchemaException class.</returns>
+        public InvalidSchemaException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the InvalidSchemaException class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        /// <returns>A new instance of the InvalidSchemaException class.</returns>
+        public InvalidSchemaException(string message, Exception inner) : base(message, inner) { }
+
+        /// <summary>
+        /// Constructs an InvalidSchemaException with the given SerializationInfo and StreamingContext.
+        /// </summary>
+        protected InvalidSchemaException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
