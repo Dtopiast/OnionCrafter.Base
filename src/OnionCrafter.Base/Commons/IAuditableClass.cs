ce4833c238475f678b9f67b5cf5089ee12baad57
dtopiast <dtopiast@protonmail.com>
2023-05-14 21:27:29 -0600

    Documentation of IAuditableClass

---
+++ src/OnionCrafter.Base/Commons/IAuditableClass.cs
@@ -0,0 +1,18 @@
+﻿namespace OnionCrafter.Base.Commons
+{
+    /// <summary>
+    /// Interface for auditable classes.
+    /// </summary>
+    public interface IAuditableClass
+    {
+        /// <summary>
+        /// Gets or sets the creation date.
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last updated date.
+        /// </summary>
+        public DateTime Updated { get; set; }
+    }
+}
