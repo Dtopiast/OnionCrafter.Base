ccaf7c852e39e7d534583021d7f3c9a8623acd57
dtopiast <dtopiast@protonmail.com>
2023-05-21 09:57:24 -0600

    Addition of ITemplateMethod interface

---
+++ src/OnionCrafter.Base/Commons/ITemplateMethod.cs
@@ -0,0 +1,18 @@
+﻿namespace OnionCrafter.Base.Commons
+{
+    /// <summary>
+    /// Interface for defining a template method pattern.
+    /// </summary>
+    public interface ITemplateMethod
+    {
+        /// <summary>
+        /// Abstract method to be executed before the task is executed.
+        /// </summary>
+        abstract Task OnBeforeExecuteAsync();
+
+        /// <summary>
+        /// Represents an asynchronous method that is called after the execution of a task.
+        /// </summary>
+        abstract Task OnAfterExecuteAsync();
+    }
+}
