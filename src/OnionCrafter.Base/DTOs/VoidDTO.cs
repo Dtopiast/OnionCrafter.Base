933e2b81f8376d878357cd60b67a86e0dc27140f
Dtopiast <dtopiast@protonmail.com>
2023-06-18 23:37:21 -0600

    Creation of VoidDTO

---
+++ src/OnionCrafter.Base/DTOs/VoidDTO.cs
@@ -0,0 +1,12 @@
+﻿using OnionCrafter.Base.Wrappers.Requests;
+using OnionCrafter.Base.Wrappers.Responses;
+
+namespace OnionCrafter.Base.DTOs
+{
+    /// <summary>
+    /// Represent a void DTO
+    /// </summary>
+    public class VoidDTO : BaseDTO, IResponseData, IRequestData
+    {
+    }
+}
